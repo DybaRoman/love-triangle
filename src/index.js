32e9a915c3ca7970367b9d7c5e3e3681e669c3e3
RomanDyba <romansmaga@mailfence.com>
2019-03-12 01:50:04 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let checked = [], a = [0];
+  for (let i = 0; i < preferences.length; i++) {
+    a[i+1] = preferences[i];
+  }
+  let number = 0;
+  for (let i = 1; i < a.length; i++){
+    if (checked.indexOf(a[i]) > 0 || i == a[i]) continue
+    else if (i == a[a[i]]) checked.push(a[i], a[a[i]]) 
+    else if (i == a[a[a[i]]]){
+      checked.push(a[i], a[a[i]], a[a[a[i]]]);
+      number++;
+    } 
+  }
+  return number;  
 };
